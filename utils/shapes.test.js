af80d77c0c6ed8487acec98d0afe647776a2887a
Kernel02 <123792001+Kernel02@users.noreply.github.com>
2023-05-06 23:57:02 -0400

    added error handling and tests for the shape classes

--- utils/shapes.test.js
+++ utils/shapes.test.js
@@ -1,8 +1,43 @@
 const shapes = require("./shapes");
 
 describe("shapes", () => {
+  describe("Triangle", () => {
+    it("should create syntax built for create an svg file of a triangle", () => {
+      const shape = new shapes.Triangle("WOW", "white", "green");
+      expect(shape.svg)
+        .toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+    <polygon points="150, 25 50, 175 250, 175" fill="green" />
+
+    <text x="150" y="135" font-size="35" text-anchor="middle" fill="white">WOW</text>
+
+    </svg>`);
+    });
+  });
   describe("Circle", () => {
-    const shape = new Circle();
-    expect(shape.render()).toEqual('<circle cx="25" cy="75" r="20" />');
+    it("should create syntax built for an svg file of a circle", () => {
+      const shape = new shapes.Circle("JMK", "blue", "orange");
+      expect(shape.svg)
+        .toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+    <circle cx="150" cy="100" r="80" fill="orange" />
+
+    <text x="150" y="120" font-size="60" text-anchor="middle" fill="blue">JMK</text>
+
+    </svg>`);
+    });
+  });
+  describe("Square", () => {
+    it("should create syntax built for an svg file of a square", () => {
+      const shape = new shapes.Square("HEY", "#000000", "#fe6f5e");
+      expect(shape.svg)
+        .toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+    <rect x="50" y="0" width="200" height="200" fill="#fe6f5e"/>
+
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="#000000">HEY</text>
+
+    </svg>`);
+    });
   });
 });
